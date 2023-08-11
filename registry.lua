b878e91a521c08c9ce88b6bb787a009253d5cfb0
Tachytaenius <henryfthomson@gmail.com>
2023-08-11 19:17:56 +0100

    Adjust enemy damages

--- registry.lua
+++ registry.lua
@@ -9,8 +9,8 @@ registry.enemies.fighter1 = {
 	shootTimerLength = 1,
 	bulletSpeed = 200,
 	bulletRadius = 1,
-	bulletDamage = 2,
-	contactDamage = 1,
+	bulletDamage = 1,
+	contactDamage = 2,
 	materialisationTime = 0.2
 }
 registry.enemies.bomber1 = {
@@ -22,7 +22,7 @@ registry.enemies.bomber1 = {
 	bulletSpeed = 250,
 	bulletRadius = 2,
 	bulletDamage = 2,
-	contactDamage = 1,
+	contactDamage = 3,
 	materialisationTime = 0.3
 }
 
