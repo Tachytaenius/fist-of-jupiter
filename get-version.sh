ed2717f853440456a7a44a0d74f692a3555b2219
Tachytaenius <henryfthomson@gmail.com>
2023-08-23 14:26:43 +0100

    Make get-version.sh indicate changed files

--- get-version.sh
+++ get-version.sh
@@ -1,1 +1,8 @@
-echo ${MAKELOVE_VERSION:-git-$(git rev-parse --short HEAD)-$(git rev-parse --abbrev-ref HEAD)}
+#!/bin/bash
+if [ -n "$MAKELOVE_VERSION" ]; then
+	echo $MAKELOVE_VERSION
+else
+	version=git-$(git rev-parse --short HEAD)-$(git rev-parse --abbrev-ref HEAD)
+	! git diff-index --quiet HEAD && version="$version-changed"
+	echo $version
+fi
